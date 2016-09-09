dc664a61beaad892494518d4dc5232ef7a9873c0
cesine <cesine@yahoo.com>
2016-09-08 21:09:31 -0400

    making app testable

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ var express = require('express'),
 
 var app = express();
 
-require('./config/express')(app, config);
+module.exports = require('./config/express')(app, config);
 
 db.sequelize
   .sync()
   .then(function () {
-    app.listen(config.port, function () {
-      console.log('Express server listening on port ' + config.port);
-    });
+    if (!module.parent) {
+      app.listen(config.port, function () {
+        console.log('Express server listening on port ' + config.port);
+      });
+    }
   }).catch(function (e) {
     throw new Error(e);
   });
